e00a3e99372dcc8e39fa306ab54757ffd19fdb9a
wangyafei <wangzifeinn@163.com>
2021-04-21 15:07:14 +0800

    refactor：joda-time

---
+++ src/jdk8/JodaTimeTest.java
@@ -0,0 +1,24 @@
+package jdk8;
+
+import org.joda.time.LocalDateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+/**
+ * @author wangyafei
+ * @date 2021/4/21 下午3:01
+ * @description joda-time 使用
+ */
+public class JodaTimeTest {
+
+    public static void main(String[] args) {
+        //增加一个月
+        DateTimeFormatter dtf = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDateTime dateTime = LocalDateTime.parse("2021-02-01 00:00:00", dtf);
+        System.out.println(dateTime.plusMonths(1).toDate());
+
+
+
+
+    }
+}
