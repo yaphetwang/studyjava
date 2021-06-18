c80d1a6e001a573b396a2f87c9bef79bf85dd4bc
wangyafei <wangzifeinn@163.com>
2021-06-18 15:47:28 +0800

    refactor：stream

--- src/jdk8/stream/StreamTest2.java
+++ src/jdk8/stream/StreamTest2.java
@@ -2,6 +2,7 @@ package jdk8.stream;
 
 import com.google.common.collect.Lists;
 
+import java.math.BigInteger;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -20,15 +21,22 @@ public class StreamTest2 {
         list = list.stream().map(String::toUpperCase).collect(Collectors.toList());
         System.out.println(list);
 
+        List<Long> longs = Lists.newArrayList(111L);
+        List<BigInteger> bigIntegers = longs.stream().map(BigInteger::valueOf).collect(Collectors.toList());
+        System.out.println(bigIntegers);
+
+
         //filter,对原始 Stream 进行某项测试，通过测试的元素被留下来生成一个新 Stream
         List<Integer> list1 = Lists.newArrayList(1, 2, 3, 4, 5, 6);
         list1.stream().filter(a -> (a < 4)).forEach(System.out::print);
+
 
         //forEach 是终止操作，因此它执行后，Stream 的元素就被“消费”掉了，
         // 你无法对一个 Stream 进行两次 terminal 运算
         Stream stream = list.stream();
         stream.forEach(System.out::println);
 //        stream.forEach(System.out::print);
+
 
         //reduce, 主要作用是把 Stream 元素组合起来。它提供一个起始值（种子），然后依照运算规则
         //（BinaryOperator），和前面 Stream 的第一个、第二个、第 n 个元素组合。
@@ -44,7 +52,6 @@ public class StreamTest2 {
         System.out.println(sum2);
 
         //
-
 
 
     }
