42bc3a6c340c376759876517d7cbc34f0b25ebac
wangyafei <wangzifeinn@163.com>
2022-02-15 15:34:07 +0800

    refactor：two sun 算法

---
+++ src/algorithm/TwoSumTest.java
@@ -0,0 +1,39 @@
+package algorithm;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author wangyafei
+ * @date 2022/2/15 3:18 下午
+ * @description 给定一个数组，给定一个数字。返回数组中可以相加得到指定数字的两个索引。
+ * 比如：给定nums = [2, 7, 11, 15], target = 9
+ * 那么要返回 [0, 1]，因为2 + 7 = 9
+ */
+public class TwoSumTest {
+
+    public static void main(String[] args) {
+
+        int[] a = new int[]{2, 7, 11, 15};
+        int[] r = twoSum(a, 26);
+
+        System.out.println(Arrays.toString(r));
+
+    }
+
+    static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                System.out.println(map);
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+
+}
--- src/sometests/DateOperate.java
+++ src/sometests/DateOperate.java
@@ -1,26 +1,33 @@
 package sometests;
 
+import com.google.common.collect.Lists;
+
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 public class DateOperate {
 
-	public static void main(String[] args) {
-		String today = "2017-01-08";  
-		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");  
-		Date date = null;  
-		try {  
-		    date = format.parse(today);  
-		} catch (Exception e) {  
-		    e.printStackTrace();  
-		}  
-		  
-		Calendar calendar = Calendar.getInstance();  
+    public static void main(String[] args) {
+        String today = "2017-01-08";
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = null;
+        try {
+            date = format.parse(today);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Calendar calendar = Calendar.getInstance();
 //		calendar.setFirstDayOfWeek(Calendar.MONDAY);  
-		calendar.setTime(date); 
-		  
-		System.out.println(calendar.get(Calendar.WEEK_OF_YEAR));  
-	}
+        calendar.setTime(date);
+
+        System.out.println(calendar.get(Calendar.WEEK_OF_YEAR));
+
+
+        List<String> list = Lists.newArrayList("1", "2", "3", "4");
+        System.out.println(String.join("-", list));
+    }
 
 }
